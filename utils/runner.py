829ddf96a66ddc4b6d9099cc1a0afbe01e9462ea
Yevhenii Orlov <orlovowork@gmail.com>
2023-02-05 22:20:03 +0200

    REFACTORING: Refactored imput/output format

--- utils/runner.py
+++ utils/runner.py
@@ -7,8 +7,8 @@ def run_solution(task_obj: Task, inputs: Any) -> None:
     diff_level = task_obj.__module__.split('.')[0]
     task_num = task_obj.task_num
     description = task_obj.__doc__
-    outputs = [task_obj.solve(case) for case in inputs]
-
+    outputs = "\n".join([str(result) for result in [task_obj.solve(case) for case in inputs]])
+    inputs = "\n".join([str(inp) for inp in inputs])
     data = [[diff_level, task_num, description, inputs, outputs]]
     headers = ["Difficulty level", "Task #", "Description", "Inputs", "Outputs"]
 
